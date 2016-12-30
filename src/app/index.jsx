a0b2df27e1e42431d3978445e12cd7638fb98f33
lushijie <lushijie1218@126.com>
2016-12-30 15:46:50 +0800

    fill textarea

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -7,32 +7,31 @@ import {rootRoute} from './router'
 import {Router, browserHistory} from 'react-router'
 import {syncHistoryWithStore, routerReducer} from 'react-router-redux'
 
-const store = createStore(counterReducer, 10);
+// const store = createStore(counterReducer, 10);
+// console.log(store.getState())
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <Router history={browserHistory} routes={rootRoute} />
+//   </Provider>,
+//   document.getElementById('app')
+// );
+
+
+
+const reducers = combineReducers({
+  ...counterReducer,
+  routing: routerReducer
+})
+
+const store = createStore(reducers, {initialState: 10})
+const history = syncHistoryWithStore(browserHistory, store)
+
 console.log(store.getState())
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={browserHistory} routes={rootRoute} />
+    <Router history={history} routes={rootRoute} />
   </Provider>,
   document.getElementById('app')
 );
-
-// const reducers = combineReducers({
-//   ...counterReducer,
-//   routing: routerReducer
-// })
-
-// const store = createStore(reducers, {initialState: 10})
-// console.log(store.getState())
-
-
-// const history = syncHistoryWithStore(browserHistory, store)
-
-
-
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <Router history={history} routes={rootRoute} />
-//   </Provider>,
-//   document.getElementById('app')
-// );
